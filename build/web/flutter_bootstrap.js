c705c379e7f29d0408c7b0faa42b5c92214f925d
mouha-byte <mouhanedmliki6@outlook.com>
2025-12-10 22:37:47 +0100

    h

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "614713094"
+    serviceWorkerVersion: "3545728716"
   }
 });
